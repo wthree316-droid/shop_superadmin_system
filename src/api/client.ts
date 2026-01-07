49d00f5f61844534d2c2dc6d6a09361a653fba85
wthree <wthree316@gmail.com>
2026-01-07 15:56:36 +0700

    Update API client to use env variable

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1', // URL ของ FastAPI
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
